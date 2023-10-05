9df0747c0039b8300674f31d44a70f6cf7134808
SergioBaron1 <sbaronrivera@gmail.com>
2023-10-05 00:01:20 -0500

    commit

---
+++ index.js
@@ -0,0 +1,5 @@
+// Obtener el formulario y el campo de nombre
+const formulario = document.getElementById("register");
+console.log(formulario)
+const campoNombre = document.getElementById("nombre");
+
