8e567d37f9580d0db752a65209cbd531906951ea
SergioBaron1 <99368452+SergioBaron1@users.noreply.github.com>
2023-10-05 17:24:26 -0500

    Create sadsad.js

---
+++ sadsad.js
@@ -0,0 +1,51 @@
+const formulario = document.getElementById('register');
+const campoNombre = document.getElementById('nombre');
+const campoApellido = document.getElementById('apellidos')
+const campoDireccion= document.getElementById('direccion')
+const campoCCUsuario = document.getElementById('ccusuario')
+const campoCCpaswd = document.getElementById('ccpaswd')
+const campoEmail=document.getElementById('email')
+formulario.addEventListener('submit', function (event) {
+ 
+  if (campoNombre.value === '' || campoNombre.value.length > 25) {
+    
+    alert('El campo de nombre debe estar lleno y tener un máximo de 25 caracteres.');
+
+    event.preventDefault();
+  }
+  if (campoApellido.value==='' || campoApellido.value.length >25){
+    alert('El campo apellido debe estar lleno y tener un máximo de 25 caracteres');
+    event.preventDefault();
+  }
+
+  const direccionRegex = /^(cll|cra|av|anv|trans)/i;
+  if(!direccionRegex.test(campoDireccion.value)){
+    alert("La direccion debe comenzar con 'cll', 'cra', 'av', 'anv' o 'trans'")
+    event.preventDefault();
+  }
+
+  const ccusuarioValue = campoCCUsuario.value;
+  const ccusuarioRegex = /^[a-zA-Z0-9]{10,20}&/;
+  if(ccusuarioValue===""){
+    alert("El campo CCUsuario debe estar lleno")
+  }else if(!ccusuarioRegex.test(ccusuarioValue)){
+    alert("El campo CCUsuario debe contener solo letras y numeros, con una longitud de 10 a 20 caracteres ")
+    event.preventDefault();
+  }
+
+  const ccpaswdValue=campoCCpaswd.value;
+  const ccpaswdRegex=/^[a-zA-Z0-9][#,%,/,&]{15,20}&/
+  if(ccpaswdValue===""){
+    alert("El campo CCpaswd debe estar lleno")
+  }else if(!ccpaswdRegex.test(ccpaswdValue)){
+    alert("El campo CCpaswd debe contener mayúsculas, numeros, letras y/o los siguientes caracteres [#,%,/,&]")
+  }
+
+
+
+
+
+
+});
+
+
